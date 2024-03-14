dba52a976732253b6a6bf27bd9a1fb3b3493fccf
Mehebul02 <mehebul2123@gmail.com>
2024-03-15 00:59:32 +0600

    price credit added

--- src/Components/Courses/Course.jsx
+++ src/Components/Courses/Course.jsx
@@ -1,15 +1,32 @@
 import PropTypes from 'prop-types';
 const Course = ({item}) => {
-    const {photo}=item
+    const {photo,name,description,price,credit}=item
     return (
         <div>
-             <div className="card w-96 bg-base-100 shadow-xl">
+             <div className="card w-96 bg-base-100 shadow-xl ">
   <figure><img src={photo} alt="Shoes" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+    <h2 className="card-title">{name}</h2>
+    <p>{description}</p>
+    <div className='flex  items-center'>
+    <p className='flex items-center'>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+Price:{price}
+    </p>
+<p className='flex items-center'>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+</svg>
+Credit:{credit}
+</p>
+
+
+
+    </div>
+    <div className="card-actions justify-center">
+      <button className="btn btn-primary text-xl font-semibold">Selected</button>
     </div>
   </div>
 </div>
--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -12,7 +12,7 @@ const Courses = () => {
   },[]);
     return (
       
-          <div className='grid grid-cols-3 gap-6'>
+          <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
             {
               courses.map((item,index) => <Course key={index} item={item}></Course>)
             }
